5b700e9e17b648a60bb1df543eebcf61b69ff7f2
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-24 15:46:14 +0530

    convertion string numbered value into integer

--- src/str_to_int.java
+++ src/str_to_int.java
@@ -2,7 +2,9 @@ import javax.swing.*;
 
 public class str_to_int {
     public static void main(String[] args) {
-        String number = JOptionPane.showInputDialog("enter the number in the form of a number")
+        String number = JOptionPane.showInputDialog("enter the number in the form of a number");
+        int no = Integer.parseInt(number);
+        //
 
     }
 
