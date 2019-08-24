75812a1b9265d1799214e238c852ec2b7ec529f8
Tushar Saxena <tushar.saxena_iot18@gla.ac.in>
2019-08-24 15:49:04 +0530

    to check if it is converted or not we will add an integer value to the input

--- src/str_to_int.java
+++ src/str_to_int.java
@@ -4,7 +4,10 @@ public class str_to_int {
     public static void main(String[] args) {
         String number = JOptionPane.showInputDialog("enter the number in the form of a number");
         int no = Integer.parseInt(number);
-        //
+        //coverting string to integer
+        // to check if it is converted or not we will add an integer value to it
+
+
 
     }
 
